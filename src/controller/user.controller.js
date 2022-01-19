8f199ecb8933ec5abb6f2fcda2f12566e8df22e8
Divyansh Rai <51046729+DivyanshRai27@users.noreply.github.com>
2022-01-19 16:24:10 +0530

    fetch emails from db

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -40,4 +40,44 @@ controller.getUser = async (req, res, next) => {
   }
 };
 
+controller.sendEmail = async (req, res, next) => {
+  try {
+    //   const user = await models.User.findAll({
+    //     attributes: ["email"],
+    //   });
+    //   for (let i = 0; i < user.length; i++) {
+    //     console.log(user[i].email);
+    //   }
+    //   res.json(user[1]);
+    cron.schedule("*/2 * * * * *", () => {
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "divyanshrai27@gmail.com",
+          pass: "",
+        },
+      });
+      var mailOptions = {
+        from: "divyanshrai27@gmail.com",
+        to: "divyanshrai0077@gmail.com",
+        subject: "Sending email",
+        text: `Hello how are you`,
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+    });
+  } catch (error) {
+    return res.json({
+      success: true,
+      message: error.message,
+      data: {},
+    });
+  }
+};
+
 module.exports = controller;
--- src/routes/user.js
+++ src/routes/user.js
@@ -8,36 +8,11 @@ const client = require("../utils/redis");
 // Model
 const models = require("../models");
 const controller = require("../controller/user.controller");
+const { json } = require("body-parser");
 
 //routes
 router.get("/", controller.getUser);
 router.post("/register", controller.registerUser);
-
-router.get("/cron-job", (req, res) => {
-  cron.schedule("*/10 * * * * *", () => {
-    var transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "divyanshrai27@gmail.com",
-        pass: "",
-      },
-    });
-
-    var mailOptions = {
-      from: "divyanshrai27@gmail.com",
-      to: "divyanshrai0077@gmail.com",
-      subject: "Sending email",
-      text: `Hello how are you`,
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
-  });
-});
+router.get("/cron-job", controller.sendEmail);
 
 module.exports = router;
