296dcce6a7ca99f2b7a72a0434f7915c6b5cfe9c
Divyansh Rai <51046729+DivyanshRai27@users.noreply.github.com>
2022-01-19 14:46:07 +0530

    cron job

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const cron = require("node-cron");
+var nodemailer = require("nodemailer");
 
 const client = require("../utils/redis");
 
@@ -10,5 +12,32 @@ const controller = require("../controller/user.controller");
 //routes
 router.get("/", controller.getUser);
 router.post("/register", controller.registerUser);
+
+router.get("/cron-job", (req, res) => {
+  cron.schedule("*/10 * * * * *", () => {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "divyanshrai27@gmail.com",
+        pass: "",
+      },
+    });
+
+    var mailOptions = {
+      from: "divyanshrai27@gmail.com",
+      to: "divyanshrai0077@gmail.com",
+      subject: "Sending email",
+      text: `Hello how are you`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  });
+});
 
 module.exports = router;
