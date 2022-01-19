741e3925b4815e974d8a98771a732c40c6e64c4f
Divyansh Rai <51046729+DivyanshRai27@users.noreply.github.com>
2022-01-19 17:23:25 +0530

    swagger integration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { Sequelize } = require("sequelize");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CRUD App",
+      version: "1.0.0",
+      description: "CRUD application for posts",
+      servers: ["http://localhost:3000"],
+    },
+  },
+  apis: ["./routes/*.js"],
+};
 
 const router = express.Router();
 
@@ -8,6 +23,9 @@ const router = express.Router();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.use("/", require("./routes/user"));
