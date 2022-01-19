aab32774fa75e6f76df07a6133ebdbc96c282801
Divyansh Rai <51046729+DivyanshRai27@users.noreply.github.com>
2022-01-19 11:53:12 +0530

    some corrections

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,9 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const { Sequelize } = require("sequelize");
 const router = express.Router();
-var sequelize = require("sequelize");
-
 const controller = require("../controller/post.controller");
 
 //Models
@@ -16,16 +12,6 @@ router.post("/", controller.registerPost);
 // Update post
 router.post("/:id", controller.updatePost);
 // Delete post
-// router.get("/:id", async (req, res) => {
-//   await models.Post.destroy({
-//     where: {
-//       id: req.params.id,
-//     },
-//   })
-//     .then((success) => res.redirect("/posts"))
-//     .catch((err) => res.render("error", { error: err.message }));
-// });
-
 router.get("/:id", controller.deletePost);
 
 module.exports = router;
